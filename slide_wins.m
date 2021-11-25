c87021784cd6976cab2355d424b99091409b53b5
pmbriley <45860615+pmbriley@users.noreply.github.com>
2021-11-25 23:29:39 +0000

    Add files via upload

--- slide_wins.m
+++ slide_wins.m
@@ -1,4 +1,4 @@
-function [slide, times] = slide_wins(bursts,events,params)
+function [slide, times] = slide_wins(bursts,events,params,showFig)
 % Paul M Briley 18/04/2021 (brileypm@gmail.com)
 % slide_wins - version 1.0
 %
@@ -6,7 +6,7 @@ function [slide, times] = slide_wins(bursts,events,params)
 % Regional Brain Correlates of Beta Bursts in Health and Psychosis: A Concurrent Electroencephalography and Functional Magnetic Resonance Imaging Study
 % Biological Psychiatry: Cognitive Neuroscience and Neuroimaging. Published online ahead of print. doi: 10.1016/j.bpsc.2020.10.018
 %
-% [slide, times] = slide_wins(bursts,events,params)
+% [slide, times] = slide_wins(bursts,events,params,showFig)
 %
 % Matlab function for calculating mean beta burst rate in sliding time
 % windows relative to a set of event markers (see Fig. 3 in the above paper)
@@ -22,17 +22,20 @@ function [slide, times] = slide_wins(bursts,events,params)
 % params.slideOver: start and end of the time range to be analysed (secs)
 % params.slideWindow: width of sliding window (secs)
 % params.slideStep: sliding window will move in these steps (secs)
+% showFig: if True then will display a plot of burst rate over time
 %
 % OUTPUTS
 % slide: vector giving mean burst rate over time
 % times: corresponding time points
 %
 
+if nargin<4; showFig = false; end
 if nargin<3; params = ''; end
 if nargin<2; error('[slide, times] = slide_wins(bursts,events,params)'); end
+if ~exist('burst_rate.m','file'); error('requires burst_rate.m'); end
 
 if ~isfield(params,'slideOver')
-    params.slideOver = [-5 5]; % time range to be analysed (seconds)
+    params.slideOver = [-3 3]; % time range to be analysed (seconds)
     disp('using default for params.slideOver');
 end
 if ~isfield(params,'slideWindow')
@@ -49,4 +52,13 @@ slide = nan(1,length(times));
 for w = 1:length(times)
     t = times(w);
     slide(w) = burst_rate(bursts,events,[t-(params.slideWindow/2) t+(params.slideWindow/2)]);
+end
+
+if showFig
+    figure;
+    plot(times,slide,'k-','linewidth',2); hold on;
+    xlabel('Time relative to events (seconds)','FontSize',16);
+    ylabel('Mean bursts per second','FontSize',16);
+    set(gca,'FontSize',14);
+    plot([0 0],get(gca,'YLim'),'k--','linewidth',1);
 end
